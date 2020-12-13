1971962d633d5027011d950c4d64e767b75c160e
Carl Stahmer <cstahmer@ucdavis.edu>
2020-12-13 14:45:28 -0800

    Copied files over from git workshop tutorial

---
+++ gen_html.R
@@ -0,0 +1,5 @@
+#rmarkdown::render("./intro_nlp_health.Rmd", output_dir="./docs/", output_file="intro.html")
+#rmarkdown::render("./dtm_tfidf_pca.Rmd", output_dir="./docs/", output_file="code.html")
+rmarkdown::render("~/workspaces/workshops/workshop-introduction-to-git/combined.Rmd", output_dir="~/workspaces/workshops/workshop-introduction-to-git/docs/", output_file="index.html")
+rmarkdown::render("~/workspaces/workshops/workshop-introduction-to-git/complete.Rmd", output_dir="~/workspaces/workshops/workshop-introduction-to-git/docs/", output_file="complete.html")
+
