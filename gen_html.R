e59de1d6eadc0c7406f8b13098dd9474f38f3d87
Tyler Shoemaker <tshoemaker@ucdavis.edu>
2021-01-13 19:36:43 -0500

    removed commented-out code (from other projects) to clarify which line compiles the course reader

--- gen_html.R
+++ gen_html.R
@@ -1,5 +1,1 @@
-#rmarkdown::render("./intro_nlp_health.Rmd", output_dir="./docs/", output_file="intro.html")
-#rmarkdown::render("./dtm_tfidf_pca.Rmd", output_dir="./docs/", output_file="code.html")
 rmarkdown::render("./index.Rmd", output_dir="./docs/", output_file="index.html")
-rmarkdown::render("~/workspaces/workshops/workshop-introduction-to-git/complete.Rmd", output_dir="~/workspaces/workshops/workshop-introduction-to-git/docs/", output_file="complete.html")
-
