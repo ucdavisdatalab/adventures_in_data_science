efe4ca03e54fbb9404490b0ed300ffa4d3979e1a
Nick Ulle <nick.ulle@gmail.com>
2021-02-22 17:29:28 -0800

    Add caching for lesson 13.
    This commit adds a function `.read_html_cache` to the `_common.R` file.
    The function attempts to read an HTML file from a local cache directory
    before and instead of downloading it from the web. If the file is not
    cached, the function downloads and caches the file.

--- _common.R
+++ _common.R
@@ -48,3 +48,27 @@ url44 <- "./img/chronicling_america_example.png"
 
 library(knitr)
 library(stringr)
+
+
+.read_html_cache = function(url, cache = "html_cache") {
+  # This function reads an HTML document from a local cache directory. If the
+  # document is not already in the cache, this function downloads and caches
+  # it.
+  if (!(require("xml2") && require("rvest")))
+    stop("read_html_cache requires packages 'xml2' and 'rvest'.")
+
+  if (!dir.exists(cache))
+    dir.create(cache)
+
+  fname = paste0(basename(url), ".html")
+  path = file.path(cache, fname)
+
+  if (file.exists(path)) {
+    doc = read_html(path)
+  } else {
+    doc = read_html(url)
+    write_xml(doc, path)
+  }
+
+  doc
+}
